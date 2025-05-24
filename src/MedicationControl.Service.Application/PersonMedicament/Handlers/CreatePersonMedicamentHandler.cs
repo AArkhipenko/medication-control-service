d6b2617702a09a853c4ec9263f4fadecf93a5ae4
AArkhipenko <arkhipenko.alex95@gmail.com>
2025-05-24 12:35:07 +0500

    [AKP-14] Добавлен автомаппер в создании

--- src/MedicationControl.Service.Application/PersonMedicament/Handlers/CreatePersonMedicamentHandler.cs
+++ src/MedicationControl.Service.Application/PersonMedicament/Handlers/CreatePersonMedicamentHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AArkhipenko.Core.Logging;
+using AutoMapper;
 using MediatR;
 using MedicationControl.Service.Application.PersonMedicament.Commands;
 using MedicationControl.Service.Domain.Repositories;
@@ -15,19 +16,23 @@ namespace MedicationControl.Service.Application.PersonMedicament.Handlers
 	internal class CreatePersonMedicamentHandler : LoggerWrapper, IRequestHandler<CreatePersonMedicamentCommand, int>
     {
 		private readonly IPersonMedicamentRepository _repository;
+		private readonly IMapper _mapper;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="CreatePersonMedicamentHandler"/> class.
 		/// </summary>
 		/// <param name="repository"><see cref="IPersonMedicamentRepository"/></param>
+		/// <param name="mapper"><see cref="IMapper"/></param>
 		/// <param name="logger"><see cref="ILogger"/></param>
 		/// <exception cref="ArgumentNullException">Не задан один из входных параметров</exception>
 		public CreatePersonMedicamentHandler(
 			IPersonMedicamentRepository repository,
+			IMapper mapper,
 			ILogger<CreatePersonMedicamentHandler> logger)
 			: base(logger)
 		{
 			this._repository = repository ?? throw new ArgumentNullException(nameof(repository));
+			this._mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
 		}
 
 		/// <inheritdoc/>
@@ -35,13 +40,7 @@ namespace MedicationControl.Service.Application.PersonMedicament.Handlers
 		{
 			using (_ = base.BeginLoggingScope())
 			{
-				var model = new DomainExt.PersonMedicament()
-				{
-					UserId = request.UserId,
-					MedicamentTypeId = request.Request.MedicamentTypeId,
-					StartDate = request.Request.StartDate,
-					EndDate = request.Request.EndDate
-				};
+				var model = this._mapper.Map<DomainExt.PersonMedicament>(request);
 
 				return this._repository.CreateAsync(model, cancellationToken);
 			}
